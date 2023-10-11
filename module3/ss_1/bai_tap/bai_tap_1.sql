eeeae5b8cc66668ad9700039e48799ada82abeca
thanhdat5987 <thanhdat5987@gmail.com>
2023-10-11 14:31:00 +0700

    update bt1

--- module3/ss_1/bai_tap/bai_tap_1.sql
+++ module3/ss_1/bai_tap/bai_tap_1.sql
@@ -1,19 +1,55 @@
 create database student_management;
 use student_management;
-create table student(id int primary key auto_increment, `name` varchar(200), age int, country varchar(50));
-create table class(id int primary key auto_increment, `name` varchar(50));
-create table teacher(id int primary key auto_increment, `name` varchar(200), age int, country varchar(50));
+CREATE TABLE student (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(200),
+    age INT,
+    country VARCHAR(50)
+);
+CREATE TABLE class (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(50)
+);
+CREATE TABLE teacher (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(200),
+    age INT,
+    country VARCHAR(50)
+);
 insert into student(name,age, country) value ("Dat", 18, "Viet Nam");
 insert into student(name,age, country) value ("Toai", 20, "Viet Nam");
 insert into class(name) value ("C0723G1");
 insert into class(name) value ("C0823G1");
 insert into teacher(name, age, country)value("Phuong",25,"USA");
 insert into teacher(name, age, country)value("Chanh",35,"USA");
-select * from student;
-select * from class;
-select * from teacher;
-delete from student where id =1;
-select  * from teacher where name ="Phuong" ;
-update teacher set country ="Viet Nam" where id="1";
-select * from teacher;
+SELECT 
+    *
+FROM
+    student;
+SELECT 
+    *
+FROM
+    class;
+SELECT 
+    *
+FROM
+    teacher;
+DELETE FROM student 
+WHERE
+    id = 1;
+SELECT 
+    *
+FROM
+    teacher
+WHERE
+    name = 'Phuong';
+UPDATE teacher 
+SET 
+    country = 'Viet Nam'
+WHERE
+    id = '1';
+SELECT 
+    *
+FROM
+    teacher;
 
