2026f3af0d0fe7e67c858eea550e920af7ebcb7f
thanhdat5987 <thanhdat5987@gmail.com>
2023-10-11 15:44:17 +0700

    update bai tap ss1

--- module3/ss_1/bai_tap/bai_tap_1.sql
+++ module3/ss_1/bai_tap/bai_tap_1.sql
@@ -1,27 +1,27 @@
 create database student_management;
 use student_management;
 CREATE TABLE student (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    `name` VARCHAR(200),
-    age INT,
-    country VARCHAR(50)
+    student_id INT PRIMARY KEY AUTO_INCREMENT,
+    student_name VARCHAR(200),
+    student_age INT,
+    student_country VARCHAR(50)
 );
 CREATE TABLE class (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    `name` VARCHAR(50)
+    class_id INT PRIMARY KEY AUTO_INCREMENT,
+    class_name VARCHAR(50)
 );
 CREATE TABLE teacher (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    `name` VARCHAR(200),
-    age INT,
-    country VARCHAR(50)
+    teacher_id INT PRIMARY KEY AUTO_INCREMENT,
+    teacher_name VARCHAR(200),
+    teacher_age INT,
+    teacher_country VARCHAR(50)
 );
-insert into student(name,age, country) value ("Dat", 18, "Viet Nam");
-insert into student(name,age, country) value ("Toai", 20, "Viet Nam");
-insert into class(name) value ("C0723G1");
-insert into class(name) value ("C0823G1");
-insert into teacher(name, age, country)value("Phuong",25,"USA");
-insert into teacher(name, age, country)value("Chanh",35,"USA");
+insert into student(student_name,student_age, student_country) value ("Dat", 18, "Viet Nam");
+insert into student(student_name,student_age, student_country) value ("Toai", 20, "Viet Nam");
+insert into class(class_name) value ("C0723G1");
+insert into class(class_name) value ("C0823G1");
+insert into teacher(teacher_name, teacher_age, teacher_country)value("Phuong",25,"USA");
+insert into teacher(teacher_name, teacher_age, teacher_country)value("Chanh",35,"USA");
 SELECT 
     *
 FROM
@@ -36,18 +36,18 @@ FROM
     teacher;
 DELETE FROM student 
 WHERE
-    id = 1;
+    student_id = 1;
 SELECT 
     *
 FROM
     teacher
 WHERE
-    name = 'Phuong';
+    teacher_name = 'Phuong';
 UPDATE teacher 
 SET 
-    country = 'Viet Nam'
+    teacher_country = 'Viet Nam'
 WHERE
-    id = '1';
+    teacher_id = '1';
 SELECT 
     *
 FROM
