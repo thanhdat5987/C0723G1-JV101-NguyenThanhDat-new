e2458af86087e0eb46fc39ebb8485cc56e04a3ae
thanhdat5987 <thanhdat5987@gmail.com>
2023-10-12 17:36:40 +0700

    update

--- module3/case_study_1/furama_management.sql
+++ module3/case_study_1/furama_management.sql
@@ -1,99 +1,110 @@
 create database if not exists quan_ly_furama;
 use quan_ly_furama;
-create table vi_tri(
-ma_vi_tri int primary key auto_increment,
-ten_vi_tri varchar(45) not null unique
+CREATE TABLE vi_tri (
+    ma_vi_tri INT PRIMARY KEY AUTO_INCREMENT,
+    ten_vi_tri VARCHAR(45) NOT NULL UNIQUE
 );
-create table trinh_do(
-ma_trinh_do int primary key auto_increment,
-ten_trinh_do varchar(45)
+CREATE TABLE trinh_do (
+    ma_trinh_do INT PRIMARY KEY AUTO_INCREMENT,
+    ten_trinh_do VARCHAR(45)
 );
-create table bo_phan(
-ma_bo_phan int primary key auto_increment,
-ten_bo_phan varchar(45) not null unique
+CREATE TABLE bo_phan (
+    ma_bo_phan INT PRIMARY KEY AUTO_INCREMENT,
+    ten_bo_phan VARCHAR(45) NOT NULL UNIQUE
 );
-create table nhan_vien(
-ma_nhan_vien int primary key auto_increment,
-ho_ten varchar(45) not null,
-ngay_sinh date not null,
-so_cmnd varchar(45) not null,
-luong double not null,
-so_dien_thoai varchar(45) not null,
-email varchar(45)not null,
-dia_chi varchar(45)not null,
-ma_vi_tri int not null,
-ma_trinh_do int not null,
-ma_bo_phan int not null,
-foreign key (ma_vi_tri) references vi_tri (ma_vi_tri),
-foreign key(ma_trinh_do) references trinh_do(ma_trinh_do),
-foreign key(ma_bo_phan) references bo_phan(ma_bo_phan)
+CREATE TABLE nhan_vien (
+    ma_nhan_vien INT PRIMARY KEY AUTO_INCREMENT,
+    ho_ten VARCHAR(45) NOT NULL,
+    ngay_sinh DATE NOT NULL,
+    so_cmnd VARCHAR(45) NOT NULL,
+    luong DOUBLE NOT NULL,
+    so_dien_thoai VARCHAR(45) NOT NULL,
+    email VARCHAR(45) NOT NULL,
+    dia_chi VARCHAR(45) NOT NULL,
+    ma_vi_tri INT NOT NULL,
+    ma_trinh_do INT NOT NULL,
+    ma_bo_phan INT NOT NULL,
+    FOREIGN KEY (ma_vi_tri)
+        REFERENCES vi_tri (ma_vi_tri),
+    FOREIGN KEY (ma_trinh_do)
+        REFERENCES trinh_do (ma_trinh_do),
+    FOREIGN KEY (ma_bo_phan)
+        REFERENCES bo_phan (ma_bo_phan)
 );
-create table loai_khach(
-ma_loai_khach int primary key auto_increment,
-ten_loai_khach varchar(45) not null
+CREATE TABLE loai_khach (
+    ma_loai_khach INT PRIMARY KEY AUTO_INCREMENT,
+    ten_loai_khach VARCHAR(45) NOT NULL
 );
-create table khach_hang(
-ma_khach_hang int primary key auto_increment,
-ma_loai_khach int not null,
-foreign key (ma_loai_khach) references loai_khach (ma_loai_khach),
-ho_ten varchar(45) not null,
-ngay_sinh date not null,
-gioi_tinh BIT(1) not null,
-so_cmnd varchar(45) not null unique,
-so_dien_thoai varchar(45) not null unique,
-email varchar(45) not null unique,
-dia_chi varchar(45)
+CREATE TABLE khach_hang (
+    ma_khach_hang INT PRIMARY KEY AUTO_INCREMENT,
+    ma_loai_khach INT NOT NULL,
+    FOREIGN KEY (ma_loai_khach)
+        REFERENCES loai_khach (ma_loai_khach),
+    ho_ten VARCHAR(45) NOT NULL,
+    ngay_sinh DATE NOT NULL,
+    gioi_tinh BIT(1) NOT NULL,
+    so_cmnd VARCHAR(45) NOT NULL UNIQUE,
+    so_dien_thoai VARCHAR(45) NOT NULL UNIQUE,
+    email VARCHAR(45) NOT NULL UNIQUE,
+    dia_chi VARCHAR(45)
 );
-create table loai_dich_vu(
-ma_loai_dich_vu int primary key auto_increment,
-ten_loai_dich_vu varchar(45) not null unique
+CREATE TABLE loai_dich_vu (
+    ma_loai_dich_vu INT PRIMARY KEY AUTO_INCREMENT,
+    ten_loai_dich_vu VARCHAR(45) NOT NULL UNIQUE
 );
-create table kieu_thue(
-ma_kieu_thue int primary key auto_increment,
-ten_kieu_thue varchar(45) not null unique
+CREATE TABLE kieu_thue (
+    ma_kieu_thue INT PRIMARY KEY AUTO_INCREMENT,
+    ten_kieu_thue VARCHAR(45) NOT NULL UNIQUE
 );
-create table dich_vu(
-ma_dich_vu int primary key auto_increment,
-ten_dich_vu varchar(45) not null unique,
-dien_tich int not null,
-chi_phi_thue double not null,
-so_nguoi_toi_da int not null,
-ma_kieu_thue int not null,
-ma_loai_dich_vu int not null,
-foreign key (ma_kieu_thue)references kieu_thue(ma_kieu_thue),
-foreign key (ma_loai_dich_vu)references loai_dich_vu(ma_loai_dich_vu),
-tieu_chuan_phong varchar(45) not null,
-mo_ta_tien_nghi_khac varchar(45),
-dien_tich_ho_boi double ,
-so_tang int,
-dich_vu_mien_phi_di_kem text
+CREATE TABLE dich_vu (
+    ma_dich_vu INT PRIMARY KEY AUTO_INCREMENT,
+    ten_dich_vu VARCHAR(45) NOT NULL UNIQUE,
+    dien_tich INT NOT NULL,
+    chi_phi_thue DOUBLE NOT NULL,
+    so_nguoi_toi_da INT NOT NULL,
+    ma_kieu_thue INT NOT NULL,
+    ma_loai_dich_vu INT NOT NULL,
+    FOREIGN KEY (ma_kieu_thue)
+        REFERENCES kieu_thue (ma_kieu_thue),
+    FOREIGN KEY (ma_loai_dich_vu)
+        REFERENCES loai_dich_vu (ma_loai_dich_vu),
+    tieu_chuan_phong VARCHAR(45) NOT NULL,
+    mo_ta_tien_nghi_khac VARCHAR(45),
+    dien_tich_ho_boi DOUBLE,
+    so_tang INT,
+    dich_vu_mien_phi_di_kem TEXT
 );
-create table hop_dong(
-ma_hop_dong int primary key auto_increment,
-ngay_lam_hop_dong datetime not null,
-ngay_ket_thuc datetime not null,
-tien_dat_coc double not null,
-ma_nhan_vien int not null,
-ma_khach_hang int not null,
-ma_dich_vu int not null,
-foreign key (ma_nhan_vien) references nhan_vien(ma_nhan_vien),
-foreign key (ma_khach_hang) references khach_hang(ma_khach_hang),
-foreign key (ma_dich_vu) references dich_vu(ma_dich_vu)
+CREATE TABLE hop_dong (
+    ma_hop_dong INT PRIMARY KEY AUTO_INCREMENT,
+    ngay_lam_hop_dong DATETIME NOT NULL,
+    ngay_ket_thuc DATETIME NOT NULL,
+    tien_dat_coc DOUBLE NOT NULL,
+    ma_nhan_vien INT NOT NULL,
+    ma_khach_hang INT NOT NULL,
+    ma_dich_vu INT NOT NULL,
+    FOREIGN KEY (ma_nhan_vien)
+        REFERENCES nhan_vien (ma_nhan_vien),
+    FOREIGN KEY (ma_khach_hang)
+        REFERENCES khach_hang (ma_khach_hang),
+    FOREIGN KEY (ma_dich_vu)
+        REFERENCES dich_vu (ma_dich_vu)
 );
-create table dich_vu_di_kem(
-ma_dich_vu_di_kem int auto_increment primary key,
-ten_dich_vu_di_kem varchar(45) not null unique,
-gia double not null,
-don_vi varchar(10) not null,
-trang_thai varchar(45)
+CREATE TABLE dich_vu_di_kem (
+    ma_dich_vu_di_kem INT AUTO_INCREMENT PRIMARY KEY,
+    ten_dich_vu_di_kem VARCHAR(45) NOT NULL UNIQUE,
+    gia DOUBLE NOT NULL,
+    don_vi VARCHAR(10) NOT NULL,
+    trang_thai VARCHAR(45)
 );
-create table hop_dong_chi_tiet(
-ma_hop_dong_chi_tiet int primary key auto_increment,
-ma_hop_dong int not null,
-ma_dich_vu_di_kem int not null,
-foreign key(ma_hop_dong) references hop_dong(ma_hop_dong),
-foreign key(ma_dich_vu_di_kem) references dich_vu_di_kem(ma_dich_vu_di_kem),
-so_luong int not null
+CREATE TABLE hop_dong_chi_tiet (
+    ma_hop_dong_chi_tiet INT PRIMARY KEY AUTO_INCREMENT,
+    ma_hop_dong INT NOT NULL,
+    ma_dich_vu_di_kem INT NOT NULL,
+    FOREIGN KEY (ma_hop_dong)
+        REFERENCES hop_dong (ma_hop_dong),
+    FOREIGN KEY (ma_dich_vu_di_kem)
+        REFERENCES dich_vu_di_kem (ma_dich_vu_di_kem),
+    so_luong INT NOT NULL
 );
 INSERT INTO vi_tri (ten_vi_tri)
 VALUES
