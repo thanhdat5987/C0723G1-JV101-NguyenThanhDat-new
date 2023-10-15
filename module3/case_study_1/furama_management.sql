97c9b47f7124420c6bd36add0c4bcee5a154e2f3
thanhdat5987 <thanhdat5987@gmail.com>
2023-10-15 22:30:58 +0700

    up date case study task6-11

--- module3/case_study_1/furama_management.sql
+++ module3/case_study_1/furama_management.sql
@@ -265,5 +265,26 @@ FROM
     dich_vu_di_kem ON dich_vu_di_kem.ma_dich_vu_di_kem = hop_dong_chi_tiet.ma_dich_vu_di_kem
 GROUP BY hop_dong_chi_tiet.ma_hop_dong
     ) AS temp ON hop_dong.ma_hop_dong = temp.ma_hop_dong;
-  
+  /*6.Hiển thị ma_dich_vu, ten_dich_vu, dien_tich, chi_phi_thue, ten_loai_dich_vu 
+  của tất cả các loại dịch vụ chưa từng được khách hàng thực hiện đặt từ quý 1 của năm 2021 (Quý 1 là tháng 1, 2, 3).
+  */
+  select dich_vu.ma_dich_vu, dich_vu.ten_dich_vu, dich_vu.dien_tich, dich_vu.chi_phi_thue, loai_dich_vu.ten_loai_dich_vu
+  from dich_vu 
+  left join loai_dich_vu on dich_vu.ma_loai_dich_vu = loai_dich_vu.ma_loai_dich_vu
+  left join hop_dong on dich_vu.ma_dich_vu = hop_dong.ma_dich_vu
+  where (dich_vu.ma_dich_vu not in(select dich_vu.ma_dich_vu from dich_vu join hop_dong on dich_vu.ma_dich_vu = hop_dong.ma_dich_vu where(year(ngay_lam_hop_dong)=2021 and month(ngay_lam_hop_dong)in (1,2,3))) 
+  and hop_dong.ngay_lam_hop_dong not in(select hop_dong.ngay_lam_hop_dong where (year(ngay_lam_hop_dong)=2021 and month(ngay_lam_hop_dong)in (1,2,3))))
+  group by dich_vu.ma_dich_vu
+  ;
+
+/*7. Hiển thị thông tin ma_dich_vu, ten_dich_vu, dien_tich, so_nguoi_toi_da, chi_phi_thue, ten_loai_dich_vu của tất cả các loại dịch vụ 
+đã từng được khách hàng đặt phòng trong năm 2020 nhưng chưa từng được khách hàng đặt phòng trong năm 2021.*/
+
+/*8.Hiển thị thông tin ho_ten khách hàng có trong hệ thống, với yêu cầu ho_ten không trùng nhau.
+Học viên sử dụng theo 3 cách khác nhau để thực hiện yêu cầu trên.*/
+
+/*9.Thực hiện thống kê doanh thu theo tháng, nghĩa là tương ứng với mỗi tháng trong năm 2021 thì sẽ có bao nhiêu khách hàng thực hiện đặt phòng.*/
+
+/*10.Hiển thị thông tin tương ứng với từng hợp đồng thì đã sử dụng bao nhiêu dịch vụ đi kèm. Kết quả hiển thị bao gồm ma_hop_dong, ngay_lam_hop_dong, ngay_ket_thuc, tien_dat_coc, so_luong_dich_vu_di_kem (được tính dựa trên việc sum so_luong ở dich_vu_di_kem).
+*/
 
