53431248364a3a0cdeb72995734c2ce120cd0f2a
thanhdat5987 <thanhdat5987@gmail.com>
2023-10-17 20:38:36 +0700

    update case study

--- module3/case_study_1/furama_management.sql
+++ module3/case_study_1/furama_management.sql
@@ -433,6 +433,79 @@ WHERE
                 AND YEAR(hop_dong.ngay_lam_hop_dong) = 2021))
 GROUP BY hop_dong.ma_hop_dong;
 
+/*13.	Hiển thị thông tin các Dịch vụ đi kèm được sử dụng nhiều nhất bởi các Khách hàng đã đặt phòng. 
+(Lưu ý là có thể có nhiều dịch vụ có số lần sử dụng nhiều như nhau).*/
 
-
-
+SELECT 
+    hop_dong.ma_hop_dong,
+    nhan_vien.ho_ten,
+    khach_hang.ho_ten,
+    khach_hang.so_dien_thoai,
+    loai_dich_vu.ten_loai_dich_vu,
+    SUM(dich_vu_di_kem.ma_dich_vu_di_kem) AS so_luong_dich_vu_di_kem,
+    hop_dong.tien_dat_coc
+FROM
+    hop_dong
+        LEFT JOIN
+    nhan_vien ON hop_dong.ma_nhan_vien = nhan_vien.ma_nhan_vien
+        LEFT JOIN
+    khach_hang ON hop_dong.ma_khach_hang = khach_hang.ma_khach_hang
+        LEFT JOIN
+    dich_vu ON hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
+        LEFT JOIN
+    loai_dich_vu ON dich_vu.ma_loai_dich_vu = loai_dich_vu.ma_loai_dich_vu
+        LEFT JOIN
+    hop_dong_chi_tiet ON hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
+        LEFT JOIN
+    dich_vu_di_kem ON hop_dong_chi_tiet.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem
+    
+    /* 14.	Hiển thị thông tin tất cả các Dịch vụ đi kèm chỉ mới được sử dụng một lần duy nhất. Thông tin hiển thị bao gồm ma_hop_dong, ten_loai_dich_vu, ten_dich_vu_di_kem, so_lan_su_dung 
+    (được tính dựa trên việc count các ma_dich_vu_di_kem).*/
+    SELECT 
+    hop_dong.ma_hop_dong,
+    nhan_vien.ho_ten,
+    khach_hang.ho_ten,
+    khach_hang.so_dien_thoai,
+    loai_dich_vu.ten_loai_dich_vu,
+    SUM(dich_vu_di_kem.ma_dich_vu_di_kem) AS so_luong_dich_vu_di_kem,
+    hop_dong.tien_dat_coc
+FROM
+    hop_dong
+        LEFT JOIN
+    nhan_vien ON hop_dong.ma_nhan_vien = nhan_vien.ma_nhan_vien
+        LEFT JOIN
+    khach_hang ON hop_dong.ma_khach_hang = khach_hang.ma_khach_hang
+        LEFT JOIN
+    dich_vu ON hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
+        LEFT JOIN
+    loai_dich_vu ON dich_vu.ma_loai_dich_vu = loai_dich_vu.ma_loai_dich_vu
+        LEFT JOIN
+    hop_dong_chi_tiet ON hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
+        LEFT JOIN
+    dich_vu_di_kem ON hop_dong_chi_tiet.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem
+    
+    /*15.	Hiển thi thông tin của tất cả nhân viên bao gồm ma_nhan_vien, ho_ten, ten_trinh_do, ten_bo_phan, 
+    so_dien_thoai, dia_chi mới chỉ lập được tối đa 3 hợp đồng từ năm 2020 đến 2021.*/
+    
+    SELECT 
+    hop_dong.ma_hop_dong,
+    nhan_vien.ho_ten,
+    khach_hang.ho_ten,
+    khach_hang.so_dien_thoai,
+    loai_dich_vu.ten_loai_dich_vu,
+    SUM(dich_vu_di_kem.ma_dich_vu_di_kem) AS so_luong_dich_vu_di_kem,
+    hop_dong.tien_dat_coc
+FROM
+    hop_dong
+        LEFT JOIN
+    nhan_vien ON hop_dong.ma_nhan_vien = nhan_vien.ma_nhan_vien
+        LEFT JOIN
+    khach_hang ON hop_dong.ma_khach_hang = khach_hang.ma_khach_hang
+        LEFT JOIN
+    dich_vu ON hop_dong.ma_dich_vu = dich_vu.ma_dich_vu
+        LEFT JOIN
+    loai_dich_vu ON dich_vu.ma_loai_dich_vu = loai_dich_vu.ma_loai_dich_vu
+        LEFT JOIN
+    hop_dong_chi_tiet ON hop_dong.ma_hop_dong = hop_dong_chi_tiet.ma_hop_dong
+        LEFT JOIN
+    dich_vu_di_kem ON hop_dong_chi_tiet.ma_dich_vu_di_kem = dich_vu_di_kem.ma_dich_vu_di_kem
