25f6a0e94e308cc5dc0bbe2e60ee6b72a9e7ced6
thanhdat5987 <thanhdat5987@gmail.com>
2023-10-19 05:56:01 +0700

    update case study

--- module3/case_study_1/furama_management.sql
+++ module3/case_study_1/furama_management.sql
@@ -558,6 +558,25 @@ WHERE
             ma_khach_hang
         FROM
             w_khach_hang_01);
-    
-    
+
+/*20.	Hiển thị thông tin của tất cả các nhân viên và khách hàng có trong hệ thống,
+ thông tin hiển thị bao gồm id (ma_nhan_vien, ma_khach_hang), ho_ten, email, so_dien_thoai, ngay_sinh, dia_chi.*/
+SELECT 
+    ma_nhan_vien AS id,
+    ho_ten,
+    email,
+    so_dien_thoai,
+    ngay_sinh,
+    dia_chi
+FROM
+    nhan_vien 
+UNION SELECT 
+    ma_khach_hang,
+    ho_ten,
+    email,
+    so_dien_thoai,
+    ngay_sinh,
+    dia_chi
+FROM
+    khach_hang;
     
