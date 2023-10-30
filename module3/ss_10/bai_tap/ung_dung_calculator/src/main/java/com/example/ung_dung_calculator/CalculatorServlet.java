cb332937b1f3a39cb46fee635aee8f8567e7b292
thanhdat5987 <thanhdat5987@gmail.com>
2023-10-31 04:58:21 +0700

    update bt2

--- module3/ss_10/bai_tap/ung_dung_calculator/src/main/java/com/example/ung_dung_calculator/CalculatorServlet.java
+++ module3/ss_10/bai_tap/ung_dung_calculator/src/main/java/com/example/ung_dung_calculator/CalculatorServlet.java
@@ -15,11 +15,28 @@ public class CalculatorServlet extends HttpServlet{
         double secondOperand = Double.parseDouble(req.getParameter("second-operand"));
         char operator = req.getParameter("operator").charAt(0);
         try {
-            double result = Calculator.calculate(firstOperand,secondOperand,operator);
+            double result = calculate(firstOperand,secondOperand,operator);
             req.setAttribute("result", result);
         } catch (Exception e){
             req.setAttribute( "exception", e.getMessage());
         }
         req.getRequestDispatcher("result.jsp").forward(req,resp);
+    }
+    public static double calculate( double firstOperand, double secondOperand, char operator){
+        switch (operator){
+            case '+':
+                return firstOperand + secondOperand;
+            case '-':
+                return firstOperand - secondOperand;
+            case '*':
+                return firstOperand * secondOperand;
+            case '/':
+                if(secondOperand != 0)
+                    return firstOperand / secondOperand;
+                else
+                    throw new NumberFormatException("Couldn't divide zero");
+            default:
+                throw new RuntimeException("Invalid operation");
+        }
     }
 }
