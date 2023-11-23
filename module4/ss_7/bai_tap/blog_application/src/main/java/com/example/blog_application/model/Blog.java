1439ac48ae4a0db666bd5de3f3b787e6275ad2bc
thanhdat5987 <thanhdat5987@gmail.com>
2023-11-23 10:52:47 +0700

    update

--- module4/ss_7/bai_tap/blog_application/src/main/java/com/example/blog_application/model/Blog.java
+++ module4/ss_7/bai_tap/blog_application/src/main/java/com/example/blog_application/model/Blog.java
@@ -1,9 +1,9 @@
 package com.example.blog_application.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+
+import static java.awt.SystemColor.TEXT;
+import static java.awt.SystemColor.text;
 
 @Entity
 public class Blog {
@@ -11,8 +11,67 @@ public class Blog {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
+    private String author;
+    private String createDate;
+    @Column(columnDefinition = "text")
+    private String description;
+    @Column(columnDefinition = "text")
     private String content;
+    private String category;
     private String remark;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(String createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Blog(int id, String name, String author, String createDate, String description, String content, String category, String remark) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.createDate = createDate;
+        this.description = description;
+        this.content = content;
+        this.category = category;
+        this.remark = remark;
+    }
+
+    public Blog(int id, String name, String author, String createDate, String description, String content, String category) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+        this.createDate = createDate;
+        this.description = description;
+        this.content = content;
+        this.category = category;
+    }
 
     public Blog() {
     }
