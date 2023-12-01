fb1022c47151addfd7d1faa80321e3df8c32876f
thanhdat5987 <thanhdat5987@gmail.com>
2023-12-01 20:54:08 +0700

    update bt

--- module4/ss_11/bai_tap/blog_application/src/main/java/com/example/blog_application/controller/RestBlogController.java
+++ module4/ss_11/bai_tap/blog_application/src/main/java/com/example/blog_application/controller/RestBlogController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("api-blogs")
 public class RestBlogController {
     @Autowired
