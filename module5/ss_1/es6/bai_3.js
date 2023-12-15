96445b1efba155f8a9e0e5b1c36aac213ac359be
thanhdat5987 <thanhdat5987@gmail.com>
2023-12-15 15:38:02 +0700

    update bt sss1

--- module5/ss_1/es6/bai_3.js
+++ module5/ss_1/es6/bai_3.js
@@ -1,7 +1,10 @@
 // 3. Kiểm tra 1 mảng có chứa số V hay không nếu có trả về V không thì trả về "không tìm thấy" (some).
 let numbers = [-1, -2, 6, 17, 14, 0, 1, 8, 9, 10, 11, 7, 3, 4];
-if (numbers.some(number => number === -1)) {
-    console.log(-1);
-} else {
-    console.log("Khong tim thay");
+function checkV(number, array){
+    if (array.some(item => item === number)) {
+        console.log(number);
+    } else {
+        console.log("Khong tim thay");
+    }
 }
+checkV(-1,numbers)
