f7bb2363b1bc3797bfcb8667c93a6dd8d3f528db
thanhdat5987 <thanhdat5987@gmail.com>
2024-03-05 09:19:17 +0700

    update bt ss5

---
+++ module5-1/ss_5_1/bai_tap/medical_form/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MedicalForm from "./components/MedicalForm";
+
+function App() {
+  return (
+    <div className="App">
+<MedicalForm></MedicalForm>
+    </div>
+  );
+}
+
+export default App;
